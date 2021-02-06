04d4816701efa85dee474e49a7bb244eba97d239
avizen-j <avizen.j@gmail.com>
2021-02-06 16:54:11 +0200

    Added one more date parser.

--- src/main/java/lt/avizen/bankaccountmanagement/utility/SafeStringParser.java
+++ src/main/java/lt/avizen/bankaccountmanagement/utility/SafeStringParser.java
@@ -3,24 +3,31 @@ package lt.avizen.bankaccountmanagement.utility;
 import org.apache.commons.lang3.time.DateUtils;
 
 import java.text.ParseException;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeParseException;
 import java.util.Date;
 
 public final class SafeStringParser {
-    public static Date parseDate(String stringDate)
-    {
-        try
-        {
-            /// TODO: Support more formats or change the approach.
-            return DateUtils.parseDate(stringDate, "dd-MM-yyyy HH:mm:ss", "dd/MM/yyyy HH:mm");
+    
+    /// TODO: Support more formats or change the approach.
+    public static Date parseDate(String stringDate) {
+        try {
+            return DateUtils.parseDate(stringDate, "dd-MM-yyyy HH:mm:ss", "dd-MM-yyyy HH:mm:ss.S", "dd/MM/yyyy HH:mm");
         } catch (ParseException e) {
-            return null;
+            try {
+                LocalDateTime localDateTime = LocalDateTime.parse(stringDate);
+                Instant instant = localDateTime.toInstant(ZoneOffset.UTC);
+                return Date.from(instant);
+            } catch (DateTimeParseException ex) {
+                return null;
+            }
         }
     }
 
-    public static Double parseDouble(String stringDouble)
-    {
-        try
-        {
+    public static Double parseDouble(String stringDouble) {
+        try {
             return Double.parseDouble(stringDouble);
         } catch (NullPointerException | NumberFormatException ex) {
             return null;
